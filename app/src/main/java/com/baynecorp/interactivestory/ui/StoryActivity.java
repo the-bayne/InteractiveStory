ba1a07e2d8c52d72826fdfe8d4a82593b898915e
Jared Thibodeaux <admin@baynecorp.com>
2019-02-16 17:37:52 -0600

    Initial commit

---
+++ app/src/main/java/com/baynecorp/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,60 @@
+package com.baynecorp.interactivestory.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.baynecorp.interactivestory.R;
+import com.baynecorp.interactivestory.model.Page;
+import com.baynecorp.interactivestory.model.Story;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+
+    private String name;
+
+    public StoryActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyTextView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()) {
+            name = "Jared";
+        }
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        pageText = String.format(pageText, name);
+    }
+}
